947edd611b8b6bcb5b444eb97f1fc6e89bf01a5d
Johnhughes814 <8hughes.john@gmail.com>
2022-04-27 20:24:13 -0500

    update

---
+++ models/User.js
@@ -0,0 +1,64 @@
+const { Model, DataTypes } = require('sequelize');
+const bcrypt = require('bcryptjs');
+const sequelize = require('../config/connection');
+
+class User extends Model {
+    checkPassword(loginPw) {
+        return bcrypt.compareSync(loginPw, this.password);
+    }
+}
+
+User.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        username: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                //make sure there is at least 8 chars in password
+                min: 2,
+            }
+        }
+    },
+    {
+        hooks: {
+            //encrypt password
+            beforeCreate: async (newUserData) => {
+                newUserData.password = await bcrypt.hash(newUserData.password, 10);
+                return newUserData;
+            },
+            beforeBulkCreate: async (users) => {
+                //loop thru array for users to grab the password
+                for (const user of users) {
+                    const {
+                        password
+                    } = user;
+                    //hash password
+                    var saltRounds = 10;
+                    var salt = bcrypt.genSaltSync(saltRounds);
+                    var hash = await bcrypt.hashSync(password, salt);
+                    user.password = hash;
+                }
+                //return user with hashed password
+                return User;
+
+
+            },
+        },
+        sequelize,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'user',
+    }
+);
+
+module.exports = User;
