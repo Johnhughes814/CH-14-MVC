8ea7a1654b4c0a56e29a8f7a57b6f585f2f26c88
Johnhughes814 <8hughes.john@gmail.com>
2022-04-28 06:15:07 -0500

    js

---
+++ public/js/login.js
@@ -0,0 +1,28 @@
+const loginFormHandler = async (event) => {
+    event.preventDefault();
+
+    const username = document.querySelector('#username-login').value.trim();
+    const password = document.querySelector('#password-login').value.trim();
+
+    if (username && password) {
+        // console.log(username, password)
+        const response = await fetch('/api/users/login', {
+            method: 'POST',
+            body: JSON.stringify({ username, password }),
+            headers: { 'Content-Type': 'application/json' },
+        });
+        // console.log(response)
+        if (response.ok) {
+            document.location.replace('/');
+        // console.log(response)
+
+        } else {
+            // console.log(response)
+            alert('Failed to log in.');
+        }
+    }
+};
+
+
+document.querySelector('.login-form').addEventListener('submit', loginFormHandler);
+
